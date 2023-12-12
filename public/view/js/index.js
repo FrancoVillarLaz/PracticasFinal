ee12ff28939a7c4189dce0afea1e913919fb1224
lautaro321 <skoollarris9@gmail.com>
2023-12-11 23:53:08 -0300

    cambios.js

--- public/view/js/index.js
+++ public/view/js/index.js
@@ -1,5 +1,5 @@
 // comprar y carrito
-var listaproductos = [];
+/*var listaproductos = [];
 var total = 0;
 
 function comprar() {
@@ -71,7 +71,70 @@ function cancelarCompra() {
   $("#total").text("Total a pagar $0"); 
   $("#juegocarrito").empty(); 
   $("#carrito").css("visibility", "hidden"); 
+}*/
+
+
+var listaproductos = [];
+var total = 0;
+
+function comprar() {
+  var nombre = document.getElementById('nombre').textContent;
+  var precioTexto = document.getElementById('precio').textContent;
+  var precio = parseInt(precioTexto.replace('$', '').replace(',', ''), 10);
+
+  if (!isNaN(precio)) {
+    añadir(nombre, precio);
+  } else {
+    console.log('No se pudo convertir el precio a un número válido.');
+  }
 }
+
+function añadir(nombre, precio) {
+  var totalElement = $("#total");
+  var carritoElement = $("#carrito");
+  var juegoimg = document.getElementById('carrusel');
+  var imgcarrito = $("#imgcarrito");
+  var juegocarrito = $("#juegocarrito");
+
+  listaproductos = listaproductos || [];
+  listaproductos.push({ nombre: nombre, precio: precio });
+
+  total += precio;
+  totalElement.text(`Total a pagar $${total}`);
+  carritoElement.css('visibility', 'visible');
+
+  // Clona la imagen del juego y agrega al carrito
+  var juegoClonado = juegoimg.cloneNode(true);
+  imgcarrito.append(juegoClonado);
+
+  var listaProductosTexto = listaproductos.map(function (producto) {
+    return `${producto.nombre}: $${producto.precio}`;
+  });
+
+  // Actualiza el contenido de juegocarrito con saltos de línea
+  juegocarrito.html(listaProductosTexto.join('<br>'));
+}
+
+function mostrarCarrito() {
+  if (listaproductos.length > 0) {
+    var listaProductosTexto = listaproductos.map(function (item) {
+      return `${item.nombre}: $${item.precio}`;
+    });
+
+    window.alert(`Productos:\n${listaProductosTexto.join("\n")}\nTotal: $${total}`);
+  } else {
+    window.alert("No hay productos en la lista.");
+  }
+}
+
+function cancelarCompra() {
+  listaproductos = [];
+  total = 0;
+  $("#total").text("Total a pagar $0");
+  $("#juegocarrito").empty();
+  $("#carrito").css("visibility", "hidden");
+}
+
 
 
 // fin de comprar y carrito
